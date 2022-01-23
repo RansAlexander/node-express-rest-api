7b29dd5c49e09c1036009d5c12b326650243e33e
RansAlexander <r0808003@student.thomasmore.be>
2022-01-23 11:21:53 +0100

    fourth iteration

--- index.js
+++ index.js
@@ -3,17 +3,51 @@ const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser')
 const mysql = require('mysql');
+const session = require('express-session');
+const { redirect } = require('express/lib/response');
+
 const PORT = 8080
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
-app.use(cors());
-app.options('*', cors());
-app.use( bodyParser.json() );     
+// app.use(cors());
+// app.options('*', cors());
+app.use(bodyParser.json());     
 app.use(bodyParser.urlencoded({
   extended: true
 })); 
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'namejeff',
+    cookie: {
+        sameSite: true,
+        secure: false
+    }
+}))
+app.use(function(req, res, next) {
+    res.locals.id = req.session.userId;
+    res.locals.name = req.session.name;
+    next();
+  });
+
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId) {
+        res.redirect('/login');
+    }
+    else {
+        next();
+    }
+}
+const redirectHome = (req, res, next) => {
+    if (req.session.userId) {
+        res.redirect('/');
+    }
+    else {
+        next();
+    }
+}
 
 // create connection
 const db = mysql.createConnection({
@@ -32,30 +66,10 @@ db.connect((err) => {
     console.log('mysql connected')
 })
 
-// create db
-// app.get('/createdb', (req, res) => {
-//     let sql = 'CREATE DATABASE nodesql'
-//     db.query(sql, (err, result) => {
-//         if(err) {
-//             throw err;
-//         }
-//         console.log(result);
-//         res.send('Database created')
-//     });
-// });
-
-// // create table
-// app.get('/createposttable', (req, res) => {
-//     let sql = 'CREATE TABLE person(id int AUTO_INCREMENT, name VARCHAR(255), surname VARCHAR(255), age INT, gender VARCHAR(1), PRIMARY KEY(id))'
-//     db.query(sql, (err, result) => {
-//         if(err) throw err;
-//         console.log(result)
-//         res.send('Post table created')
-//     })
-// });
-
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    const {userId} = req.session
+    const {name} = req.session
+    res.render('index.ejs', {'user': userId})
 })
 
 // insert data
@@ -85,13 +99,46 @@ app.get('/persons', (req, res) => {
 })
 
 // get single persons
-app.get('/person/:id', (req, res) => {
+app.get('/person/:id', redirectLogin, (req, res) => {
     let sql = `SELECT * FROM person WHERE id = ${req.params.id}`
     db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result)
         res.render('person/show.ejs', {'data': result})
     })
+})
+
+// login page
+app.get('/login', redirectHome, (req, res) => {
+    res.render('login/index.ejs')
+});
+
+//auth login
+app.post('/login', redirectHome, (req, res) => {
+    const user = req.body.login;
+    const pass = req.body.password
+    // parameterized query to prevent sql injections
+    // passwords are not send in the query but checked in the code as extra security measure
+    // not that it matters because passwords are unhashed anyway
+    let sql = `SELECT * FROM \`user\` WHERE user=?`
+    db.query(sql, [user], (err, result) => {
+        if(err) throw err;
+        // bad login
+        if(result.length === 0 || result[0].password != req.body.password) {
+            res.render('login/index.ejs', {'data': 'Wrong login information'})
+        }
+        else {
+            req.session.userId = result[0].id;
+            req.session.name = result[0].user;
+            res.redirect('/')
+        }
+    })
+});
+
+app.get('/logout', redirectLogin, (req, res) => {
+    req.session.userId = null;
+    req.session.name = null;
+    res.redirect('/')
 })
 
 app.listen(PORT, () => {
